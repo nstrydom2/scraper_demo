759aabc4334a3dcc739d110f76bc3152a0b22003
Nick <nstrydom@gmail.com>
2020-03-23 23:30:01 -0500

    Reconfigured port

--- api.py
+++ api.py
@@ -43,5 +43,5 @@ if __name__ == '__main__':
     # Debug/Development
     # app.run(debug=True, host="0.0.0.0", port="5000")
     # Production
-    http_server = WSGIServer(('', 8000), flask_app)
+    http_server = WSGIServer(('', 5000), flask_app)
     http_server.serve_forever()
